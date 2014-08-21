abdc6f72557a407b756f3d3d7ea8fb4979163125
Kyle Crop <kylecrop@utexas.edu>
2014-08-21 10:08:55 -0500

    Time for testing.

--- lib/acts_as_priceable_on.rb
+++ lib/acts_as_priceable_on.rb
@@ -4,4 +4,13 @@ require 'acts_as_priceable_on/version'
 require 'acts_as_priceable_on/price'
 
 module ActsAsPriceableOn
+	
+	def acts_as_priceable
+		#base.include stuff
+	end
+
+	def acts_as_priceable_on
+		#base.inclu
+	end
+
 end
